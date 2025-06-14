57b45dd4934bb527a8a6f1cff96d88c1627a6b33
LoganEndreman <pengellylogan@gmail.com>
2025-06-13 19:50:25 -0700

    Add storyworld save and load

--- Card.vb
+++ Card.vb
@@ -1,10 +1,24 @@
-﻿Public Class Card
+Public Class Card
     Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
 
     End Sub
 
     Private Sub Card_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Public Function ToCardData() As CardData
+        Return New CardData With {
+            .Title = TextBox1.Text,
+            .Description = RichTextBox1.Text,
+            .Deck = DropDownButton1.Text
+        }
+    End Function
+
+    Public Sub FromCardData(data As CardData)
+        TextBox1.Text = data.Title
+        RichTextBox1.Text = data.Description
+        DropDownButton1.Text = data.Deck
     End Sub
 
 End Class
---
+++ CardData.vb
@@ -0,0 +1,5 @@
+Public Class CardData
+    Public Property Title As String
+    Public Property Description As String
+    Public Property Deck As String
+End Class
